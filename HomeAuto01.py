cab2a6753815423cd37ee9f695266b910e12eb23
Younghwan Jang <yfaney@gmail.com>
2015-06-05 16:08:40 -0500

    Adding Notification to tablets

--- HomeAuto01.py
+++ HomeAuto01.py
@@ -24,22 +24,28 @@
 import sys
 
 import Adafruit_DHT
-import gspread
+import gspread, json
 
 import subprocess, httplib, json, datetime, time
+from oauth2client.client import SignedJwtAssertionCredentials
 
 # Global Variables
 
 # Google Docs account email, password, and spreadsheet name.
-GDOCS_EMAIL            = 'YOUR_GOOGLE_EMAIL_ADDRESS'
-GDOCS_PASSWORD         = 'YOUR_GOOGLE_PASSWORD'
-GDOCS_SPREADSHEET_NAME = 'YOUR_SPREADSHEET_FILE_NAME'
+GDOCS_EMAIL            = 'YOUR_GMAIL_ID'
+GDOCS_PASSWORD         = 'YOUR_GMAIL_PASSWORD'
+GDOCS_SPREADSHEET_NAME = 'YOUR_SPREADSHEET_NAME'
 
-GCM_SERVER_API_KEY = 'YOUR_SERVER_API_KEY'
+GCM_SERVER_API_KEY = 'YOUR_GOOGLE_DEVELOPER_CONSOLE_API_KEY'
 GCM_REQUEST_URL = 'android.googleapis.com'
 GCM_REQUEST_SUBPATH = '/gcm/send'
-GCM_CLIENT_KEY = [ 'YOUR_1ST_ANDROID_PHONE_GCM_ID', 'YOUR_2ND_ANDROID_PHONE_GCM_ID' ]
+GCM_CLIENT_KEY = [ 'YOUR_1ST_CLIENT_KEY_ANDROID_FOR_1HOUR_INTERVAL', 'YOUR_2ND_CLIENT_KEY_ANDROID_FOR_1HOUR_INTERVAL' ]
+GCM_CLIENT_KEY2 = ['YOUR_1ST_CLIENT_KEY_ANDROID_FOR_2MINUTE_INTERVAL', 'YOUR_2ND_CLIENT_KEY_ANDROID_FOR_2MINUTE_INTERVAL']
 
+# For OAuth2 part!
+json_key = json.load(open('gspread_client_key.json'))
+scope = ['https://spreadsheets.google.com/feeds']
+credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
 
 # How long to wait (in seconds) between measurements.
 FREQUENCY_SECONDS      = 120
@@ -50,7 +56,10 @@ sensor_args = { '11': Adafruit_DHT.DHT11,
                                 '2302': Adafruit_DHT.AM2302 }
 
 # Functions!
+gc = None
 worksheet = None
+
+
 
 def noti_server(temp, humi, msg):
         c = httplib.HTTPSConnection(GCM_REQUEST_URL)
@@ -58,7 +67,7 @@ def noti_server(temp, humi, msg):
         headers['Content-Type'] = 'application/json'
         headers['Authorization'] = 'key=' + GCM_SERVER_API_KEY
 
-        notipayload = {'message' : msg + "(Temp={0:0.1f}*C/Humi={1:0.1f}%)".format(temp, humi), 'temp' : temp, 'humi' : humi}
+        notipayload = {'message' : msg + u"(Temp={0:0.1f}\u2103/Humi={1:0.1f}%)".format(temp, humi), 'temp' : temp, 'humi' : humi}
         notimsg = {'registration_ids': GCM_CLIENT_KEY, 'data' : notipayload}
         c.request("POST", GCM_REQUEST_SUBPATH, json.dumps(notimsg), headers)
         response = c.getresponse()
@@ -66,13 +75,40 @@ def noti_server(temp, humi, msg):
         data = response.read()
         print data
 
+def push_server(temp, humi):
+        c = httplib.HTTPSConnection(GCM_REQUEST_URL)
+        headers = {}
+        headers['Content-Type'] = 'application/json'
+        headers['Authorization'] = 'key=' + GCM_SERVER_API_KEY
+        notipayload = {'temp' : temp, 'humi' : humi }
+        notimsg = {'registration_ids':GCM_CLIENT_KEY2, 'data':notipayload }
+        c.request("POST", GCM_REQUEST_SUBPATH, json.dumps(notimsg), headers)
+        response = c.getresponse()
+        print response.status, response.reason
+        data = response.read()
+        print data
+
+def auth_open_sheet(spreadsheet):
+        """Connect to Google Docs with OAuth2 and return the first worksheet"""
+        try:
+                gc = gspread.authorize(credentials)
+                sheet = gc.open(spreadsheet).sheet1
+                return sheet
+        except Exception, e:
+                if gc is None:
+                        print 'Unable to login. Check OAuth key.'
+                else:
+                        print 'Unable to get spreadsheet. Check the spreadsheet name.'
+                print str(e)
+                sys.exit(1)
+
 def login_open_sheet(email, password, spreadsheet):
         """Connect to Google Docs spreadsheet and return the first worksheet."""
         try:
                 gc = gspread.login(email, password)
                 #print 'successfully logged in'
-                worksheet = gc.open(spreadsheet).sheet1
-                return worksheet
+                sheet = gc.open(spreadsheet).sheet1
+                return sheet
         except:
                 if gc is None:
                         print 'Unable to login.  Check email and password.'
@@ -80,21 +116,32 @@ def login_open_sheet(email, password, spreadsheet):
                         print 'Unable to get spreadsheet. Check the spreadsheet name.'
                 sys.exit(1)
 
-def send_to_google_drive(p_worksheet, p_temp, p_humi, p_msg):
+def send_to_google_drive(sheet, p_temp, p_humi, p_msg):
         # Append the data in the spreadsheet, including a timestamp
         try:
                 if p_temp is not None and p_humi is not None:
                         row = [ datetime.datetime.now(), p_temp, p_humi, p_msg ]
-                        p_worksheet.append_row(row)
+                        sheet.append_row(row)
                         #print (row)
                         print 'Wrote a row to {0}'.format(GDOCS_SPREADSHEET_NAME)
                 else:
                         print 'Something wrong. Not written.'
-        except:
+                return sheet
+        except Exception, e:
                 # Error appending data, most likely because credentials are stale.
                 # Null out the worksheet so a login is performed at the top of the loop.
                 print 'Append error, logging in again'
-                worksheet = None
+                print str(e)
+                """Append Error so retry one time"""
+                sheet = auth_open_sheet(GDOCS_SPREADSHEET_NAME)
+                row = [ datetime.datetime.now(), p_temp, p_humi, p_msg ]
+                try:
+                        sheet.append_row(row)
+                        return sheet
+                except Exception, e:
+                        """If it does not work again, make worksheet null"""
+                        print str(e)
+                        return None
 
 # Starts Here!!!
 
@@ -106,8 +153,8 @@ if len(sys.argv) == 7 and sys.argv[1] in sensor_args:
         humi_low = int(sys.argv[5])
         humi_high = int(sys.argv[6])
 else:
-        print 'usage: sudo ./HomeAuto.py [11|22|2302] GPIOpin# TEMP_LOW TEMP_HIGH HUMIDITY_LOW HUMIDITY_HIGH'
-        print 'example: sudo ./HomeAuto.py 2302 4 23 25 45 60 - Read from an AM2302 connected to GPIO #4 and notification if the temperature or humidity is out of range'
+        print 'usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#'
+        print 'example: sudo ./Adafruit_DHT.py 2302 4 23 25 45 60 - Read from an AM2302 connected to GPIO #4 and notify if the temperature or humidity is out of 23~25*C or 45~60%'
         sys.exit(1)
 
 # For GCM notification
@@ -117,7 +164,7 @@ noticount = 30
 while True:
         # Login if necessary.
         if worksheet is None:
-                worksheet = login_open_sheet(GDOCS_EMAIL, GDOCS_PASSWORD, GDOCS_SPREADSHEET_NAME)
+                worksheet = auth_open_sheet(GDOCS_SPREADSHEET_NAME)
 
         # Try to grab a sensor reading.  Use the read_retry method which will retry up
         # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
@@ -155,24 +202,26 @@ while True:
 
                 if is_temp is not None and is_humi is not None:
                         noticount = noticount + 1
-                        send_to_google_drive(worksheet, temperature, humidity, is_temp + " and " + is_humi)
+                        worksheet = send_to_google_drive(worksheet, temperature, humidity, is_temp + " and " + is_humi)
                         if noticount >= 30:
                                 noti_server(temperature, humidity, message)
                                 noticount = 0
                 elif is_temp is None and is_humi is not None:
                         noticount = noticount + 1
-                        send_to_google_drive(worksheet, temperature, humidity, is_humi)
+                        worksheet = send_to_google_drive(worksheet, temperature, humidity, is_humi)
                         if noticount >= 30:
                                 noti_server(temperature, humidity, message)
                                 noticount = 0
                 elif is_temp is not None and is_humi is None:
                         noticount = noticount + 1
-                        send_to_google_drive(worksheet, temperature, humidity, is_temp)
+                        worksheet = send_to_google_drive(worksheet, temperature, humidity, is_temp)
                         if noticount >= 30:
                                 noti_server(temperature, humidity, message)
                                 noticount = 0
                 else:
-                        send_to_google_drive(worksheet, temperature, humidity, "")
+                        worksheet = send_to_google_drive(worksheet, temperature, humidity, "")
+                push_server(temperature, humidity)
         else:
                 print 'Failed to get reading. Try again!'
         time.sleep(FREQUENCY_SECONDS)
+pi@mypifaney01 ~ $
