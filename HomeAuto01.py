56cdb7203c41dce2a539a2e83e542cc6f8333f93
Younghwan Jang <yfaney@gmail.com>
2015-10-16 11:22:25 -0500

    Weather Forecast Function Added

--- HomeAuto01.py
+++ HomeAuto01.py
@@ -29,6 +29,8 @@ import gspread, json
 import subprocess, httplib, json, datetime, time
 from oauth2client.client import SignedJwtAssertionCredentials
 
+import WeatherUG as WUG
+
 # Global Variables
 
 # Google Docs account email, password, and spreadsheet name.
@@ -37,7 +39,7 @@ GDOCS_PASSWORD         = 'YOUR_GMAIL_PASSWORD'
 GDOCS_SPREADSHEET_NAME = 'YOUR_SPREADSHEET_NAME'
 
 GCM_SERVER_API_KEY = 'YOUR_GOOGLE_DEVELOPER_CONSOLE_API_KEY'
-GCM_REQUEST_URL = 'android.googleapis.com'
+GCM_REQUEST_URL = 'gcm-http.googleapis.com'
 GCM_REQUEST_SUBPATH = '/gcm/send'
 GCM_CLIENT_KEY = [ 'YOUR_1ST_CLIENT_KEY_ANDROID_FOR_1HOUR_INTERVAL', 'YOUR_2ND_CLIENT_KEY_ANDROID_FOR_1HOUR_INTERVAL' ]
 GCM_CLIENT_KEY2 = ['YOUR_1ST_CLIENT_KEY_ANDROID_FOR_2MINUTE_INTERVAL', 'YOUR_2ND_CLIENT_KEY_ANDROID_FOR_2MINUTE_INTERVAL']
@@ -46,6 +48,11 @@ GCM_CLIENT_KEY2 = ['YOUR_1ST_CLIENT_KEY_ANDROID_FOR_2MINUTE_INTERVAL', 'YOUR_2ND
 json_key = json.load(open('gspread_client_key.json'))
 scope = ['https://spreadsheets.google.com/feeds']
 credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
+
+MYSERVER_SENSOR_NAME = 'LivingRoom01'
+MYSERVER_REQUEST_URL = '192.168.0.23'
+MYSERVER_POST_SUBPATH = '/PiHomeAutomation/api/HTSensorsApi'
+MYSERVER_FC_POST_SUBPATH = '/PiHomeAutomation/api/HTForecasts'
 
 # How long to wait (in seconds) between measurements.
 FREQUENCY_SECONDS      = 120
@@ -65,12 +72,13 @@ def noti_server(temp, humi, msg):
         c = httplib.HTTPSConnection(GCM_REQUEST_URL)
         headers = {}
         headers['Content-Type'] = 'application/json'
-        headers['Authorization'] = 'key=' + GCM_SERVER_API_KEY
+        headers['Authorization'] = 'key=' + GCM_SERVER_API_KEY2
 
         notipayload = {'message' : msg + u"(Temp={0:0.1f}\u2103/Humi={1:0.1f}%)".format(temp, humi), 'temp' : temp, 'humi' : humi}
         notimsg = {'registration_ids': GCM_CLIENT_KEY, 'data' : notipayload}
         c.request("POST", GCM_REQUEST_SUBPATH, json.dumps(notimsg), headers)
         response = c.getresponse()
+        print "Notifying to Cellphone..."
         print response.status, response.reason
         data = response.read()
         print data
@@ -87,6 +95,39 @@ def push_server(temp, humi):
         print response.status, response.reason
         data = response.read()
         print data
+
+def insert_my_server(p_temp, p_humi, p_msg):
+        c = httplib.HTTPSConnection(MYSERVER_REQUEST_URL)
+        headers = {}
+        headers['Content-Type'] = 'application/json'
+        #headers['Authorization'] = 'key=' +
+        hTSensor = {'SensorName': MYSERVER_SENSOR_NAME, 'CreatedOn': str(datetime.datetime.now()), 'Temperature': p_temp, 'Humidity': p_humi, 'Message': p_msg}
+        try:
+                c.request("POST", MYSERVER_POST_SUBPATH, json.dumps(hTSensor), headers)
+                response = c.getresponse()
+                print "Insert to Server: ", response.status, response.reason
+                data = response.read()
+                print data
+        except Exception, e:
+                print 'Unable to insert to server.'
+                print str(e)
+
+def insert_forecast_my_server(zipcode):
+        c = httplib.HTTPSConnection(MYSERVER_REQUEST_URL)
+        headers = {}
+        headers['Content-Type'] = 'application/json'
+        #headers['Authorization'] = 'key=' +
+        hTForecast = WUG.getHourlyFC_HTSensor(zipcode)
+        subpath = "%s/%s" % (MYSERVER_FC_POST_SUBPATH, str(zipcode))
+        try:
+                c.request("POST", subpath, "'%s'" % json.dumps(hTForecast), headers)
+                response = c.getresponse()
+                print "Insert to Server: ", response.status, response.reason
+                data = response.read()
+                print data
+        except Exception, e:
+                print 'Unable to insert to server.'
+                print str(e)
 
 def auth_open_sheet(spreadsheet):
         """Connect to Google Docs with OAuth2 and return the first worksheet"""
@@ -154,17 +195,25 @@ if len(sys.argv) == 7 and sys.argv[1] in sensor_args:
         humi_high = int(sys.argv[6])
 else:
         print 'usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#'
-        print 'example: sudo ./Adafruit_DHT.py 2302 4 23 25 45 60 - Read from an AM2302 connected to GPIO #4 and notify if the temperature or humidity is out of 23~25*C or 45~60%'
+        print 'example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4'
         sys.exit(1)
 
 # For GCM notification
 noticount = 30
+# For Forecast
+fccount = 30
 
 # Loop forever!
 while True:
+        #Push the forecast data into the server
+        fccount = fccount + 1
+        if fccount >= 30:
+                insert_forecast_my_server(66204)
+                fccount = 0
+
         # Login if necessary.
-        if worksheet is None:
-                worksheet = auth_open_sheet(GDOCS_SPREADSHEET_NAME)
+        #if worksheet is None:
+        #       worksheet = auth_open_sheet(GDOCS_SPREADSHEET_NAME)
 
         # Try to grab a sensor reading.  Use the read_retry method which will retry up
         # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
@@ -200,28 +249,27 @@ while True:
                 if message is not None:
                         print message
 
-                if is_temp is not None and is_humi is not None:
-                        noticount = noticount + 1
-                        worksheet = send_to_google_drive(worksheet, temperature, humidity, is_temp + " and " + is_humi)
-                        if noticount >= 30:
-                                noti_server(temperature, humidity, message)
-                                noticount = 0
-                elif is_temp is None and is_humi is not None:
-                        noticount = noticount + 1
-                        worksheet = send_to_google_drive(worksheet, temperature, humidity, is_humi)
-                        if noticount >= 30:
-                                noti_server(temperature, humidity, message)
-                                noticount = 0
-                elif is_temp is not None and is_humi is None:
-                        noticount = noticount + 1
-                        worksheet = send_to_google_drive(worksheet, temperature, humidity, is_temp)
-                        if noticount >= 30:
-                                noti_server(temperature, humidity, message)
-                                noticount = 0
+                if is_temp is None and is_humi is None:
+                        #worksheet = send_to_google_drive(worksheet, temperature, humidity, "")
+                        insert_my_server(temperature, humidity, "")
                 else:
-                        worksheet = send_to_google_drive(worksheet, temperature, humidity, "")
+                        noticount = noticount + 1
+                        if noticount >= 30:
+                                noti_server(temperature, humidity, message)
+                                noticount = 0
+                        if is_temp is not None and is_humi is not None:
+                                noticount = noticount + 1
+                                #worksheet = send_to_google_drive(worksheet, temperature, humidity, is_temp + " and " + is_humi)
+                                insert_my_server(temperature, humidity,  is_temp + " and " + is_humi)
+                        elif is_temp is None and is_humi is not None:
+                                #worksheet = send_to_google_drive(worksheet, temperature, humidity, is_humi)
+                                insert_my_server(temperature, humidity, is_humi)
+                        elif is_temp is not None and is_humi is None:
+                                noticount = noticount + 1
+                                #worksheet = send_to_google_drive(worksheet, temperature, humidity, is_temp)
+                                insert_my_server(temperature, humidity, is_temp)
                 push_server(temperature, humidity)
         else:
                 print 'Failed to get reading. Try again!'
         time.sleep(FREQUENCY_SECONDS)
-pi@mypifaney01 ~ $
+print 'HomeAuto.py Exited...'
---
+++ weatherUG.py
@@ -0,0 +1,39 @@
+# This is for Weather Underground API
+import urllib2, json
+
+REQUEST_URL_HOME = "http://api.wunderground.com"
+API_KEY = "YOUR_WUG_API_KEY"
+
+def getHourlyFC(zipcode):
+    url = "%s/api/%s/hourly/q/%s.json" % (REQUEST_URL_HOME, API_KEY, str(zipcode                                      ))
+    respJson = requestWUG(url)
+    return respJson
+
+def getHourlyFC_HTSensor(zipcode):
+    respJson = getHourlyFC(zipcode)
+    forecastJson = respJson['hourly_forecast']
+    newFormat = []
+    for item in forecastJson:
+        hourly = {}
+        hourly['epoch'] = item['FCTTIME']['epoch']
+        hourly['temp'] = item['temp']['metric']
+        hourly['humidity'] = item['humidity']
+        newFormat.append(hourly)
+    return newFormat
+
+def requestWUG(url):
+    errorCount = 10
+    while errorCount > 0:
+        #response = requests.get(url)
+        resp = urllib2.urlopen(url)
+        #str_response = response.text.decode('utf-8')
+        str_response = resp.read().decode('utf-8')
+        obj = json.loads(str_response)
+        if 'error' in obj['response']:
+            print("Error in retrieving data. Trying again...")
+            logging.error("DataRetrievalError - TWC API Error")
+            # Retrying after 1 sec
+            time.sleep(1)
+        else:
+            errorCount = 0
+    return obj
